f92a2d813ed2b36d5de28c5a596672639c61ede4
xiezhonglong1 <3335904720@qq.com>
2020-04-25 20:59:02 +0800

    yxk

---
+++ js/ddwl.js
@@ -0,0 +1,169 @@
+//topnav图片
+$('.topnav_right li').on('mousemove', function () {
+    $('.topnav_right li').find('img').hide()
+    $(this).find('img').show()
+})
+$('.topnav_right li').on('mouseout', function () {
+    $('.topnav_right li').find('img').hide()
+})
+
+//top城市站下拉链
+$('#topseach_right_pj').on('mousemove', function () {
+    $(this).find('ul').show()
+    $('#topseach_right_pj ul li').on('click', function () {
+        let text = $(this).text()
+        $('#topseach_right_pj').find('a').text(text)
+    })
+})
+$('#topseach_right_pj').on('mouseout', function () {
+    $(this).find('ul').hide()
+})
+
+
+//topseach下拉链
+$('#topseach_right_zh').on('mousemove', function () {
+    $(this).find('ul').show()
+    $('#topseach_right_zh ul li').on('click', function () {
+        let text = $(this).text()
+        $('#topseach_right_zh').find('a').text(text)
+    })
+})
+$('#topseach_right_zh').on('mouseout', function () {
+    $(this).find('ul').hide()
+})
+
+
+//topseach搜索
+$('#topseach_right_zh').siblings('button').on('click', function () {
+    $(this).siblings('input').val('')
+})
+
+//topseach搜索focus
+$('#topseach_right_input').focus(function () {
+    $('.topseach_right_focus').show()
+})
+$('#topseach_right_input').blur(function () {
+    $('.topseach_right_focus').hide()
+})
+
+//topnavlist头部导航栏list
+$(function () {
+    $('ul.nav li.nav-item').hover(function () {
+        $(this).children('div.subMenu').stop().slideToggle(100);
+    })
+})
+
+//大轮播图
+$(function () {
+    var swiper = new Swiper('.swiper-container', {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        autoplay: true,
+        autoplay: {
+            delay: 3000,//1秒切换一次
+        },
+    });
+    //鼠标覆盖停止自动切换
+    swiper.el.onmouseover = function () {
+        swiper.autoplay.stop();
+    }
+    //鼠标离开开始自动切换
+    swiper.el.onmouseout = function () {
+        swiper.autoplay.start();
+    }
+})
+
+//小轮播图
+$(function () {
+    ///轮播
+    $(function () {
+        $('#lunbobox').on('mousemove', function () {
+            $("#toleft").show()
+            $("#toright").show()
+        })
+        $('#lunbobox').on('mouseout', function () {
+            $("#toleft").hide()
+            $("#toright").hide()
+        })
+    })
+    var t;
+    var index = 0;
+    var Lenght = $('.lunbo').children().length - 1
+    /////自动播放
+    t = setInterval(play, 3000)
+    $("#lunbobox ul").children().eq(0).addClass('ulhover')
+    function play() {
+        index++;
+        if (index > Lenght) {
+            index = 0
+        }
+        $("#lunbobox ul li").eq(index).addClass('ulhover').siblings().removeClass('ulhover')
+        $(".lunbo a ").eq(index).fadeIn(1000).siblings().fadeOut(1000);
+    };
+    ///点击鼠标 图片切换
+    $("#lunbobox ul li").click(function () {
+        //添加 移除样式
+        //$(this).addClass("lito").siblings().removeClass("lito"); //给当前鼠标移动到的li增加样式 且其余兄弟元素移除样式   可以在样式中 用hover 来对li 实现
+        $(this).addClass('ulhover').siblings().removeClass('ulhover')
+        var index = $(this).index(); //获取索引 图片索引与按钮的索引是一一对应的
+        $(".lunbo a ").eq(index).fadeIn(1000).siblings().fadeOut(1000); // siblings  找到 兄弟节点(不包括自己）
+    });
+
+    /////////////上一张、下一张切换
+    $("#toleft").click(function () {
+        index--;
+        if (index < 0) { //判断index<0的情况为：开始点击#toright  index=0时  再点击 #toleft 为负数了 会出错
+            index = Lenght
+        }
+        $("#lunbobox ul li").eq(index).addClass('ulhover').siblings().removeClass('ulhover')
+        $(".lunbo a ").eq(index).fadeIn(1000).siblings().fadeOut(1000); // siblings  找到 兄弟节点(不包括自己）必须要写
+    }); // $("#imgbox a ")获取到的是一个数组集合 。所以可以用index来控制切换
+
+    $("#toright").click(function () {
+        index++;
+        if (index > Lenght) {
+            index = 0
+        }
+        $("#lunbobox ul li").eq(index).addClass('ulhover').siblings().removeClass('ulhover')
+        $(".lunbo a ").eq(index).fadeIn(1000).siblings().fadeOut(1000); // siblings  找到 兄弟节点(不包括自己）
+    });
+
+    ///////鼠标移进  移出
+    $("#lunbobox ul li,.lunbo a img,#toright,#toleft ").hover(
+        ////////鼠标移进
+        function () {
+            $('#toright,#toleft').show()
+            clearInterval(t);
+        },
+        ///////鼠标移开
+        function () {
+            t = setInterval(play, 3000)
+            function play() {
+                index++;
+                if (index > Lenght) {
+                    index = 0
+                }
+                $("#lunbobox ul li").eq(index).addClass('ulhover').siblings().removeClass('ulhover')
+                $(".lunbo a ").eq(index).fadeIn(1000).siblings().fadeOut(1000);
+            }
+        })
+})
+
+//主题玩乐
+$('.ztyw_nav ul li').eq(0).addClass('ulhover')
+$('.ztyw_nav ul li').on('click', function () {
+    $('.ztyw_nav ul li').removeClass('ulhover')
+    $(this).addClass('ulhover')
+    let index = $(this).index()
+    $('.ztyw_itmeBok').children().hide()
+    $('.ztyw_itmeBok').children().eq(index).show()
+})
